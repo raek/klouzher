4db87ad3adc97a9b7b068995f803e185955fa9f6
Rasmus Svensson <raek@lysator.liu.se>
2009-10-13 17:03:18 +0200

    added license

--- src/com/github/raek/events.clj
+++ src/com/github/raek/events.clj
@@ -1,3 +1,11 @@
+;; Copyright (c) Rasmus Svensson. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
 (ns com.github.raek.events
     (:use [clojure.contrib.core :only [dissoc-in]]))
 
@@ -11,5 +19,5 @@
   (doseq [[key-or-agent f] (-> state ::handlers event-type)]
       (if (keyword? key-or-agent)
 	(apply f args)
-	(send-off key-or-agent f args)))
+	(send key-or-agent f args)))
   state)
