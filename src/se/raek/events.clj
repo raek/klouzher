9e01e225e17a7eb342c920990cbf104810a8a7ab
Rasmus Svensson <raek@lysator.liu.se>
2010-02-02 15:11:17 +0100

    Changed the how event handlers are stored

--- src/se/raek/events.clj
+++ src/se/raek/events.clj
@@ -11,24 +11,25 @@
     
     This library is built around two concept: event dispatchers and event
     handlers. Event handlers are functions that are either run directly or run
-    by an agent. Event dispatchers store their handlers in a referece, most
-    often an agent.
+    by an agent. Event dispatchers store their handlers in a map, which may
+    be place in one of the reference types.
     
-    To create a event dispatcher, create an agent (or any other reference type)
-    with a map as its state. To add an event handler to it, send (or use
-    corresponding reference altering construct) add-event-handler to the
-    agent. Events are dispatched by sending dispatch-event to the event
-    dispatcher agent.
+    The functions ins this library are used to change the state of an event
+    dispatcher. They can be called directly with the map (containing its event
+    handlers) as its first argument directly, or be used to mutate the state
+    of a reference type with swap!, alter, commute, send or send-off.
     
-    The handler function is either sent to an agent or run by the event
-    dispatcher itself. Event handlers should be short and non-blocking, and if
-    a more time consuming or blocking operation is necessary it should be done
-    in another thread (e.g. sent to an agent with send-off).
+    When an event is dispatched, and a handler is invoked its function is
+    either sent to an agent or run by the event dispatcher itself. A event
+    handler that is run by the dispatcher should be short and non-blocking. If
+    a time consuming or blocking operation is necessary, it should be executed
+    in a separate thread (e.g. sent to an agent with send-off), since the
+    other handlers may want to be notified about the event as soon as
+    possible.
     
-    Since event dispatcher most likely contain other state than just it's event
-    handlers, this library assumes that the state of the event dispatcher is a
-    map and stores the handlers under the key :com.github.raek.events/handlers."}
-    com.github.raek.events
+    The event handler function will receive the event type as its first
+    argument followed by any arguments passed to dispatch-event."}
+    se.raek.events
     (:use [clojure.contrib.core :only [dissoc-in]]))
 
 (defn add-event-handler
@@ -37,17 +38,17 @@
   If key-or-agent is an agent, the handler function f will be sent to that
   agent when the event occurs. Otherwise, f will be run by the thread that
   dispatches the event."
-  [state event-type key-or-agent f]
-  (assoc-in state [::handlers event-type key-or-agent] f))
+  [handlers event-type key-or-agent f]
+  (assoc-in handlers [event-type key-or-agent] f))
 
-(defn remove-event-handler [state event-type key-or-agent]
+(defn remove-event-handler [handlers event-type key-or-agent]
   "Remove an event handler from an event dispatcher."
-  (dissoc-in state [::handlers event-type key-or-agent]))
+  (dissoc-in handlers [event-type key-or-agent]))
 
-(defn dispatch-event [state event-type & args]
+(defn dispatch-event [handlers event-type & args]
   "Dispatches an event."
-  (doseq [[key-or-agent f] (-> state ::handlers event-type)]
+  (doseq [[key-or-agent f] (get handlers event-type)]
       (if (keyword? key-or-agent)
-	(apply f args)
-	(apply send key-or-agent f args)))
-  state)
+	(apply f (cons event-type args))
+	(apply send key-or-agent f (cons event-type args))))
+  handlers)
